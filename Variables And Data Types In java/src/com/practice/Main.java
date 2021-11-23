616a1980630127d64b30f084573377787ee9e7f8
suardi26 <suardi260696@gmail.com>
2021-11-23 17:49:02 +0800

    Add project Variables And Data Types In Java

---
+++ Variables And Data Types In java/src/com/practice/Main.java
@@ -0,0 +1,18 @@
+package com.practice;
+
+public class Main {
+    public static void main (String[] args){
+
+        //Variabel berfungsi menampung data sesuai dengan tipe data yang ditentukan.
+        //Nama variabel yang tidak dapat digunakan yaitu keyword pada java seperti class,package,private,dll.
+        //Contoh :
+
+            double cloudC;//mendeklarasikan variabel cloudC bertipe double.
+            float thu = 4.5f;//Memberikan nilai awal variabel thu dengna nilai 4.5 dan bertipe float.
+            int ioT = 3; //Nama variabel ioT bertipe data Integer dan Bernilai 3.
+
+
+       
+
+    }
+}
