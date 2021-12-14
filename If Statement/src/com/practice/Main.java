36d828bf3d5d686468e1a4a45fb9f0178806fae8
suardi26 <suardi260696@gmail.com>
2021-12-14 23:36:56 +0800

    Create Project Operator Ternary And fix project If Statement

--- If Statement/src/com/practice/Main.java
+++ If Statement/src/com/practice/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String [] args ){
+    public static void main(String[] args){
 
         Scanner enq = new Scanner(System.in);
         int number, mod, option;
---
+++ Operator Ternary/src/com/practice/Main.java
@@ -0,0 +1,34 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args ){
+
+    /*
+     Operator dan operand
+     Operator adalah instruksi yang digunakan untuk mendapatkan hasil dari proses. Sedangkan
+     Operand adalah nilai asal yang digunakan dalam sebuah proses operasi.
+     Contoh : 5 + 8;
+     '5 dan 8' adalah operand.
+     '+' adalah operator.
+
+     Operator Ternary / Operator Kondisional adalah operator yang terdiri dari 3 operand.
+     Tujuan Operator Ternary adalah untuk menentukan suatu nilai variabel (Assignment).
+     */
+
+        Scanner enq = new Scanner(System.in);
+        System.out.println("(###############-Game Berhitung-###############)");
+        System.out.println("Silahkan jawab pertanyaan berikut : ");
+        System.out.print("(25 * 3) / 5 = ");
+
+        int ans = enq.nextInt();
+        String res = (ans == 15) ? ("BENAR") : ("SALAH");
+
+        System.out.println("\n(###############-Hasilnya-###############)\n");
+        System.out.println("(25 * 3) / 5 = 15");
+        System.out.println("Jawaban Anda "+res);
+
+    }
+}
